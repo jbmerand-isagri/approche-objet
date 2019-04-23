c0045f311ff0c9e5ceb7d1894cf834e56c8eeb1f
Jean-Baptiste Mérand <jbmerand@tutanota.com>
2019-04-23 10:33:49 +0200

    TP n°1.2

---
+++ src/main/java/fr/diginamic/entites/Client.java
@@ -0,0 +1,20 @@
+package fr.diginamic.entites;
+
+/**
+ * @author Jean-Baptiste
+ *
+ */
+public class Client {
+	int numeroCompte;
+	String nom;
+	String prenom;
+
+	public Client(int numeroCompte, String nom, String prenom) {
+		System.out.println("Nouvelle instance créée");
+		this.numeroCompte = numeroCompte;
+		this.nom = nom;
+		this.prenom = prenom;
+		System.out.println("Informations qui ont valorisé les variables d'instance : ");
+		System.out.println(numeroCompte + ", " + nom + ", " + prenom);
+	}
+}
---
+++ src/main/java/fr/diginamic/essais/TestClient.java
@@ -0,0 +1,15 @@
+package fr.diginamic.essais;
+
+import fr.diginamic.entites.Client;
+
+/**
+ * @author Jean-Baptiste
+ *
+ */
+public class TestClient {
+
+	public static void main(String[] args) {
+		Client client1 = new Client(25, "John", "Doe");
+		Client client2 = new Client(12, "Arya", "Stark");
+	}
+}
