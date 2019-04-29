80f42d172ba9ed8084c35cc923e85b924f68aeea
Jean-Baptiste Mérand <jbmerand@tutanota.com>
2019-04-29 15:24:11 +0200

    TP n°9

---
+++ src/main/java/fr/diginamic/tp9/maps/entites/Pays.java
@@ -0,0 +1,101 @@
+package fr.diginamic.tp9.maps.entites;
+
+/**
+ * Exercice 3
+ * 
+ * @author Jean-Baptiste
+ *
+ */
+public class Pays {
+	private String nom;
+	private int nbHabitants;
+	private double pibParHab;
+
+	public Pays(String nom, int nbHabitants, double pibParHab) {
+		this.nom = nom;
+		this.nbHabitants = nbHabitants;
+		this.pibParHab = pibParHab;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom
+	 *            the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the nbHabitants
+	 */
+	public int getNbHabitants() {
+		return nbHabitants;
+	}
+
+	/**
+	 * @param nbHabitants
+	 *            the nbHabitants to set
+	 */
+	public void setNbHabitants(int nbHabitants) {
+		this.nbHabitants = nbHabitants;
+	}
+
+	/**
+	 * @return the pibParHab
+	 */
+	public double getPibParHab() {
+		return pibParHab;
+	}
+
+	/**
+	 * @param pibParHab
+	 *            the pibParHab to set
+	 */
+	public void setPibParHab(double pibParHab) {
+		this.pibParHab = pibParHab;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pays other = (Pays) obj;
+		if (nbHabitants != other.nbHabitants)
+			return false;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		if (Double.doubleToLongBits(pibParHab) != Double.doubleToLongBits(other.pibParHab))
+			return false;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Pays [nom=" + nom + ", nbHabitants=" + nbHabitants + ", pibParHab=" + pibParHab + "]";
+	}
+
+}
---
+++ src/main/java/fr/diginamic/tp9/maps/essais/TestMapPays.java
@@ -0,0 +1,64 @@
+package fr.diginamic.tp9.maps.essais;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import fr.diginamic.tp9.maps.entites.Pays;
+
+/**
+ * Exercice 1
+ * 
+ * @author Jean-Baptiste
+ *
+ */
+public class TestMapPays {
+
+	public static void main(String[] args) {
+
+		Pays usa = new Pays("USA", 327167434, 55805);
+		Pays france = new Pays("France", 67795000, 38476);
+		Pays allemagne = new Pays("Allemagne", 82979100, 47589);
+		Pays uk = new Pays("UK", 65105246, 43770);
+		Pays italie = new Pays("Italie", 60483973, 29866);
+		Pays japon = new Pays("Japon", 126330302, 39058);
+		Pays chine = new Pays("Chine", 1417913092, 7989);
+		Pays russie = new Pays("Russie", 146780700, 11099);
+		Pays inde = new Pays("Inde", 1296834042, 1626);
+
+		Map<String, Pays> mapPays = new HashMap<>();
+		mapPays.put("usa", usa);
+		mapPays.put("france", france);
+		mapPays.put("allemagne", allemagne);
+		mapPays.put("uk", uk);
+		mapPays.put("italie", italie);
+		mapPays.put("japon", japon);
+		mapPays.put("chine", chine);
+		mapPays.put("russie", russie);
+		mapPays.put("inde", inde);
+
+		System.out.println("Les différentes clés de la map :");
+		// mapPays.forEach((key, value) -> System.out.println(key));
+		System.out.println(mapPays.keySet());
+
+		System.out.println("Les différentes valeurs de la map :");
+		System.out.println(mapPays.values());
+
+		// suppression pays avec le moins d'habitants
+		int minHab = Integer.MAX_VALUE;
+		String paysMinHab = "";
+
+		for (Iterator iterator = mapPays.keySet().iterator(); iterator.hasNext();) {
+			String clePays = (String) iterator.next();
+			int nbHabitants = mapPays.get(clePays).getNbHabitants();
+			if (nbHabitants < minHab) {
+				minHab = nbHabitants;
+				paysMinHab = clePays;
+			}
+		}
+		mapPays.remove(paysMinHab);
+
+		System.out.println(mapPays.keySet());
+	}
+
+}
